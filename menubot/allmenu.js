bcbe1a763f9643d12616a36d69d44d296dc49e78
Shine1103 <102493567+Shine1103@users.noreply.github.com>
2022-03-28 08:06:23 +0700

    Update allmenu.js

--- menubot/allmenu.js
+++ menubot/allmenu.js
@@ -3,10 +3,7 @@ return`━━֍  〔 🪀 _*BY ${botName}*_ 🪀〕
 ──────────────────
 ◈ _Nama Owner : ${NamaOwner}_
 ◈ _Nomor Owner : ${NomorOwner}_
-️ YT : Zeil Botz JANGAN LUPA SUBSCRIBE ️
-️ NOMOR : 081376900646 HEMAT JAN LUPA️S
-〽️ IG : ( PERBAIKAN !! ) JAN LUPA FOLLOW 〽️
-〽️ TIK TOK : ( PERBAIKAN !! ) JAN LUPA FOLLOW 〽️
+️ 
 
 
 
@@ -327,7 +324,7 @@ return`━━֍  〔 🪀 _*BY ${botName}*_ 🪀〕
 ║│${prefix}herolist「 Mobile Legends 」
 ║│${prefix}googlesearch 「 Terserah 」
 ╠══════════════════════╡
-║     By *ZeilBotz*
+║     By *ShineBot*
 ╚══════════════════════╡`
 }
 exports.menunya = menunya
